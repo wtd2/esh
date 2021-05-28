2f0ba16e35f6bf1d4404e8e82289a730ffab5e57
wtd2 <zhang.hiram@gmail.com>
2021-05-28 12:50:48 +0800

    5/28 local commit

--- src/esh/esh_print_str.c
+++
@@ -1,8 +0,0 @@
-#include "esh.h"
-
-#include <string.h>
-#include <unistd.h>
-
-void esh_print_str(char const *s, int fd) {
-    write(fd, s, strlen(s));
-}
---
+++ src/exec/exec.c
@@ -0,0 +1,4 @@
+#include "exec.h"
+
+#include <string.h>
+
