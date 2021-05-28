f51b8273b529b8df222d9598ee1812ec0378fdcd
wtd2 <zhang.hiram@gmail.com>
2021-05-28 14:28:27 +0800

    5/28 draft

--- inc/prompt.h
+++ inc/prompt.h
@@ -1,6 +1,9 @@
 #ifndef PROMPT_H
 #define PROMPT_H
 
+#include <readline/readline.h>
+#include <readline/history.h>
+
 void prompt_display(struct s_esh *shell);
 int prompt_input(struct s_esh *shell, char **raw);
 
--- src/loop.c
+++ src/loop.c
@@ -3,20 +3,30 @@
 #include "shell.h"
 #include "prompt.h"
 
+// void loop_shell(struct s_esh *shell) {
+//     char* raw = NULL;
+//     if (shell->var.is_a_tty)
+//         prompt_display(shell);
+//     int ret_value = prompt_input(shell, &raw);
+//     if (ret_value != 0) {
+//         if (ret_value == -1)
+//             shell->var.return_value = 1;
+//         shell->var.exit = 1;
+//         esh_free_str(&raw);
+//         return;
+//     }
+    
+    
+    
+//     esh_free_str(&raw);
+// }
+
+#define BUFFER_SIZE = 2048
+
 void loop_shell(struct s_esh *shell) {
-    char* raw = NULL;
-    if (shell->var.is_a_tty)
-        prompt_display(shell);
-    int ret_value = prompt_input(shell, &raw);
-    if (ret_value != 0) {
-        if (ret_value == -1)
-            shell->var.return_value = 1;
-        shell->var.exit = 1;
-        esh_free_str(&raw);
-        return;
-    }
-    
-    
-    
-    esh_free_str(&raw);
+    char line[BUFFER_SIZE] = "";
+    prompt_input(shell, line); // read input into line
+
+    //echo line
+    esh_println_str(line, 1);
 }
